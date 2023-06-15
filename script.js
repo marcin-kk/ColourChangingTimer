602fad50f5ea0369b33e0bb060620277a5ca6711
Marcin <mkula.poczta@gmail.com>
2023-06-15 07:30:43 +0200

    start button functionality added

--- script.js
+++ script.js
@@ -1,12 +1,32 @@
-const startBtn = document.querySelector('.start')
-const pauseBtn = document.querySelector('.pause')
-const stopBtn = document.querySelector('.stop')
-const resetBtn = document.querySelector('.reset')
-const historyBtn = document.querySelector('.history')
-const stopwatch = document.querySelector('.stopwatch')
-const time = document.querySelector('.time')
-const timeList = document.querySelector('.time-list')
-const infoBtn = document.querySelector('.info')
-const modalShadow = document.querySelector('.modal-shadow')
-const closeModalBtn = document.querySelector('.close')
+const startBtn = document.querySelector(".start")
+const pauseBtn = document.querySelector(".pause")
+const stopBtn = document.querySelector(".stop")
+const resetBtn = document.querySelector(".reset")
+const historyBtn = document.querySelector(".history")
+const stopwatch = document.querySelector(".stopwatch")
+const time = document.querySelector(".time")
+const timeList = document.querySelector(".time-list")
+const infoBtn = document.querySelector(".info")
+const modalShadow = document.querySelector(".modal-shadow")
+const closeModalBtn = document.querySelector(".close")
 
+let sec = 0
+let min = 0
+
+const handleStart = () => {
+	setInterval(() => {
+		sec++
+	if (sec <= 9 ){
+		stopwatch.textContent = `${min}:0${sec}`
+	} else if (sec > 9 && sec <=59){
+		stopwatch.textContent = `${min}:${sec}`
+	} else {
+		sec = 0
+		min++
+		stopwatch.textContent = `${min}:0${sec}`
+	}
+	
+	}, 200)
+}
+
+startBtn.addEventListener("click", handleStart)
