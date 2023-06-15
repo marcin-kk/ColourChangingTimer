7fded42173bda0c5f19c05789c964834c89dcadb
Marcin <mkula.poczta@gmail.com>
2023-06-15 06:42:57 +0200

    JS variables created

---
+++ script.js
@@ -0,0 +1,12 @@
+const startBtn = document.querySelector('.start')
+const pauseBtn = document.querySelector('.pause')
+const stopBtn = document.querySelector('.stop')
+const resetBtn = document.querySelector('.reset')
+const historyBtn = document.querySelector('.history')
+const stopwatch = document.querySelector('.stopwatch')
+const time = document.querySelector('.time')
+const timeList = document.querySelector('.time-list')
+const infoBtn = document.querySelector('.info')
+const modalShadow = document.querySelector('.modal-shadow')
+const closeModalBtn = document.querySelector('.close')
+
